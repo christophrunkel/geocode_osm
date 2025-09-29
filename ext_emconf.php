8dba3d056039982cdd81df19eee6699c341bb8c1
christophrunkel <32742371+christophrunkel@users.noreply.github.com>
2025-09-29 12:54:02 +0200

    Add files via upload

---
+++ ext_emconf.php
@@ -0,0 +1,18 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'OpenStreetMap Geocoding Task',
+    'description' => 'This extension provides a task that generates tt_address geocoding information via OpenStreetMap instead of GoogleMaps',
+    'category' => 'services',
+    'author' => 'Christoph Runkel',
+    'author_email' => 'dialog@christophrunkel.de',
+    'state' => 'alpha',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '12.4.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
