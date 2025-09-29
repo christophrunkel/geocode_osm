08cf53beed344914c9f6449babddc1bd685a32db
christophrunkel <32742371+christophrunkel@users.noreply.github.com>
2025-09-29 14:01:51 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,6 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
+            'tt_address' => '8.0.0-9.9.9',
         ],
         'conflicts' => [],
         'suggests' => [],
